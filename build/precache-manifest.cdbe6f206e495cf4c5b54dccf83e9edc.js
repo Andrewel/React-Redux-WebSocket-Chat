e23995ef6d2bc25d27da46a4c1ddb0cb8b7b9bc2
Andrew <vlason15@gmail.com>
2019-08-15 19:18:03 +0300

    feat: service worker reg

---
+++ build/precache-manifest.cdbe6f206e495cf4c5b54dccf83e9edc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3e34ca48d199ac6c7c44",
+    "url": "/static/js/main.3e34ca48.chunk.js"
+  },
+  {
+    "revision": "08f5938dd4f73c4112d7",
+    "url": "/static/js/1.08f5938d.chunk.js"
+  },
+  {
+    "revision": "3e34ca48d199ac6c7c44",
+    "url": "/static/css/main.37836ac3.chunk.css"
+  },
+  {
+    "revision": "003e82f506c0b9d58b539865e221f3d1",
+    "url": "/index.html"
+  }
+];
